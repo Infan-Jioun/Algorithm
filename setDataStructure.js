91f829aa949a58713cdab83f068104306a25e68a
infan11 <infanjiounrahman20606@gmail.com>
2025-10-31 09:54:28 +0600

    file name change SetDataStructure and add set example

--- setDataStructure.js
+++ setDataStructure.js
@@ -1,3 +1,9 @@
 //Set Defination
 // Set is a collection of data unique elements, where order doesn't matter and duplicate are not allowed
 //একটা মান একবার যোগ করলে, একই মান আবার দিলে সেটা ignore করে দেয়।
+// Example 
+const set = new Set();
+set.add("infan");
+set.add("jioun");
+set.add("rahman");
+console.log(set.size);
