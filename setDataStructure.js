459d34c7b1ab1e48327894ca937f4922927def92
infan11 <infanjiounrahman20606@gmail.com>
2025-10-31 10:02:07 +0600

    Example added useing array and update defination

--- setDataStructure.js
+++ setDataStructure.js
@@ -1,9 +1,19 @@
 //Set Defination
 // Set is a collection of data unique elements, where order doesn't matter and duplicate are not allowed
 //একটা মান একবার যোগ করলে, একই মান আবার দিলে সেটা ignore করে দেয়।
-// Example 
+// set moloto use hoie view count like count er somi etc
+// Example-1
+// const set = new Set();
+// set.add("infan");
+// set.add("jioun");
+// set.add("rahman");
+// console.log(set.size);
+// example-2
+const infan = { userName: "Infan" };
+const jioun = {userName : "Jioun"};
+const rahman = {userName : "Rahman"};
+
 const set = new Set();
 set.add("infan");
 set.add("jioun");
-set.add("rahman");
-console.log(set.size);
+console.log(set);
