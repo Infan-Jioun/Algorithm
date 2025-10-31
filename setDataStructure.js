6c84ef4b4820f1062b53cface2f90396d488e09c
infan11 <infanjiounrahman20606@gmail.com>
2025-10-31 10:23:30 +0600

    Example-3 added set.add() and Array.Form() defination

--- setDataStructure.js
+++ setDataStructure.js
@@ -2,18 +2,30 @@
 // Set is a collection of data unique elements, where order doesn't matter and duplicate are not allowed
 //একটা মান একবার যোগ করলে, একই মান আবার দিলে সেটা ignore করে দেয়।
 // set moloto use hoie view count like count er somi etc
+//Set এবং Array আলাদা data structure —
+// set.add() হলো Set object - এর একটা built -in method,
+//     যেটা দিয়ে তুমি Set - এর মধ্যে নতুন element যোগ করতে পারো।
+//Array.from() মূলত কোনো iterable বা array-like object কে সাধারণ array তে রূপান্তর করে।
 // Example-1
 // const set = new Set();
-// set.add("infan");
+// set.add("infan"); // new elemrent add kore 
 // set.add("jioun");
 // set.add("rahman");
 // console.log(set.size);
-// example-2
-const infan = { userName: "Infan" };
-const jioun = {userName : "Jioun"};
-const rahman = {userName : "Rahman"};
+// // example-2
+// const infan = { userName: "Infan" };
+// const jioun = { userName: "Jioun" };
+// const rahman = { userName: "Rahman" };
 
-const set = new Set();
-set.add("infan");
-set.add("jioun");
-console.log(set);
+// const set = new Set();
+// set.add("infan");
+// set.add("jioun");
+// console.log(set);
+
+// Example-3
+const arr = ["laptop", "mobile", "mouse"];
+const set = new Set(arr);
+const test = Array.from(set); // Array.from() মূলত কোনো iterable বা array-like object কে সাধারণ array তে রূপান্তর করে।
+test.push("keyboard")
+console.log(test)
+    ;
