3220f44694dc33321766e679fde75f8bd5e1041f
infan11 <infanjiounrahman20606@gmail.com>
2025-11-02 18:12:12 +0600

    added arr flat

--- array.js
+++ array.js
@@ -1,1 +1,12 @@
 // nested Array flast 
+// ১. flat() কী করে?
+
+// Array.prototype.flat() হচ্ছে array flattening method।
+// মানে — nested array (array এর ভিতরে array) কে একটা single level array বানায়।
+const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9, [10, 11]]]]]
+// console.log(arr)
+//Infinity দিলে — যত গভীর nested array-ই থাকুক না কেন,
+// সব লেভেল খুলে একদম single flat array বানাবে।
+const flatArray = arr.flat(Infinity)
+console.log(flatArray);
+
