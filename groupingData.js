3e65b35e49794939e707cce8a7eab62c9fb4b837
infan11 <infanjiounrahman20606@gmail.com>
2025-11-03 15:18:29 +0600

    addded Denormalizing file

--- groupingData.js
+++ groupingData.js
@@ -31,9 +31,54 @@ const count = surveyResponses.reduce((table, response) => {
     // } else {
     //     table[response] = 1   
     // }
-    table[response] = (table[response] || 0 ) + 1
+    table[response] = (table[response] || 0) + 1
     return table
 }, {})
-console.log(count);
+// console.log(count);
 //? Output
 // { A: 5, C: 3, B: 5, D: 1 }
+
+
+//* Grouping and Aggregating Data
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+
+const sales = [
+    { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+    { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+    { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+    { category: "Home", item: "Chair", price: 150, quantity: 1 },
+    { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+    { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
+];
+
+const totalSalesByCategory = sales.reduce((table, sale) => {
+    const { category, price, quantity } = sale;
+    if (!table[category]) {
+        table[category] = {
+            totalRevenue: 0,
+            itemCount: 0
+        }
+    }
+    table[category].totalRevenue  += price * quantity;
+    table[category].itemCount += quantity
+ 
+    return table;
+}, {})
+console.log(totalSalesByCategory);
+//? Output
+// {
+//   Electronics: {
+//     totalRevenue: 1330,
+//     itemCount: 4,
+//   },
+//   Books: {
+//     totalRevenue: 110,
+//     itemCount: 3,
+//   },
+//   Home: {
+//     totalRevenue: 150,
+//     itemCount: 1,
+//   },
+// };
