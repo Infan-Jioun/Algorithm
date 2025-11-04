f5cf30e9074f3bdd87841da4948048a35eff9538
infan11 <infanjiounrahman20606@gmail.com>
2025-11-04 07:26:59 +0600

    added stack workflow

--- stack.js
+++ stack.js
@@ -1,3 +1,42 @@
 // Stack JavaScript er modde 2 type implement kora jai
-// 1. Array
+// 1. Array 3 method in stack (push , pop, peek)
+//push data add 
+//pop data remove,
+//peek data infront
 // 2. Linkelist
+// constructor হলো একটা special method যা class থেকে নতুন object (instance) বানানোর সময় স্বয়ংক্রিয়ভাবে কল হয়।
+
+class Stack {
+    constructor() {
+        this.items = []
+    }
+    push(value) {
+        this.items.push(value)
+    }
+    pop() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items.pop();
+    }
+    peek() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.items.length - 1]
+    }
+    isEmpty(){
+        return this.items.length === 0
+    }
+    print(){
+        console.log(this.items.slice().reverse().join("👉"));
+    }
+}
+const stack = new Stack();
+stack.push(10)
+stack.push(20)
+stack.push(30)
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
