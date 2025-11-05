97d16406b30c719e8591c3e8bfa1e78dc1a059ce
infan11 <infanjiounrahman20606@gmail.com>
2025-11-05 11:30:01 +0600

    added append

--- linkeList.js
+++ linkeList.js
@@ -6,9 +6,25 @@ class Node {
 }
 class LinkeList {
     constructor(value) {
-       this.head = null;
-       this.tail = null;
-       this.length = 0;
-        
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
     }
+    append(value) {
+        const newNode = new Node(value);
+        // if the linkedlist empty
+        if (this.head) {
+            this.head = newNode;
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++
+    }
+    prepend() { }
+    indsert() { }
+    remove() { }
+    print() { }
 }
