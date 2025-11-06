4515d8eacb6e5fa1f5277188f0de5b6b771f3b7b
infan11 <infanjiounrahman20606@gmail.com>
2025-11-06 15:17:33 +0600

    added pop

--- problem/Solution/stack.js
+++ problem/Solution/stack.js
@@ -1,10 +1,17 @@
 export default class Stack {
     constructor() {
         this.items = []
-    } 
+    }
     // o(n)
     push(value) {
         this.items.push(value)
     }
-    
-}
+
+    pop() {
+        if (this.isEmpty) {
+            return undefined;
+        }
+        return this.items.map()
+    }
+     
+ }
