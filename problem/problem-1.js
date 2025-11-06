08b9a4cb5ce62b36414f629ad5cee95a8a182fac
infan11 <infanjiounrahman20606@gmail.com>
2025-11-06 12:12:28 +0600

    added expensive task funtion

--- problem/problem-1.js
+++ problem/problem-1.js
@@ -3,3 +3,15 @@
 // Implement a simple in-memory cache for an "expensive" function (like a database query or API call).
 // The goal is to store the results of a function call so that if the same call is made again,
 // the result is returned from the cache instead of running the expensive function.
+
+// 
+const expensiveTask = (id) => {
+    console.log(`Run the id : ${id}`);
+    return {
+        id: id, Data: `Some data for id : ${id} `, tempstamp: new Date().getTime()
+    }
+}
+const getData = (id) => {
+    return expensiveTask(id)
+}
+console.log(getData(123));
