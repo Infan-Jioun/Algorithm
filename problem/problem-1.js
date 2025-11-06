a41275ba55e7c9b75db94c63f8923225d05cb391
infan11 <infanjiounrahman20606@gmail.com>
2025-11-06 12:26:20 +0600

    New implement how to create a new cache

--- problem/problem-1.js
+++ problem/problem-1.js
@@ -4,7 +4,8 @@
 // The goal is to store the results of a function call so that if the same call is made again,
 // the result is returned from the cache instead of running the expensive function.
 
-// 
+// create new a dataCache
+const dataCache = new Map();
 const expensiveTask = (id) => {
     console.log(`Run the id : ${id}`);
     return {
@@ -12,6 +13,18 @@ const expensiveTask = (id) => {
     }
 }
 const getData = (id) => {
-    return expensiveTask(id)
+    if (dataCache.has(id)) {
+        console.log("Cache HiT The id ", id);
+        return dataCache.get(id);
+    }
+    console.log("Cache miss for id ", id);
+
+
+    const data = expensiveTask(id)
+    dataCache.set(id, data)
+    return data
+
 }
+console.log(dataCache);
 console.log(getData(123));
+console.log(dataCache);
